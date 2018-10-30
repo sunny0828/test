788657a2ac33c8887b84f83f45f3422983fc13c3
sunny0828 <18916211539@163.com>
2018-10-30 15:29:52 +0800

    two

---
+++ src/main/java/com/lby/gitProject/Two.java
@@ -0,0 +1,13 @@
+package com.lby.gitProject;
+
+public class Two {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+}
